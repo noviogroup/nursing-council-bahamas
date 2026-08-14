945b66a858032bf827b8ebab09a48c6c2cc03807
Valdez <noviogroup@gmail.com>
2026-08-14 16:10:57 -0400

    Restore baseline Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
@@ -90,5 +89,5 @@ export default {
       },
     }
   },
-  plugins: [animate],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
